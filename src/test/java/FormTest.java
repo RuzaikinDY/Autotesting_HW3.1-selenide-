3847181c4850a8b1b24ec804a77323a07bc41746
RuzaikinDY <Devilwind@mail.ru>
2022-07-31 06:06:11 +0300

    initial commit

---
+++ src/test/java/FormTest.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FormTest {
+    @BeforeEach
+    void openBrowser() {
+        open("http://localhost:9999/");
+    }
+
+    // POSITIVE
+    @Test
+    void shouldSendForm() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("Иванов Иван");
+        form.$("[data-test-id='phone'] input").setValue("+79271520804");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
