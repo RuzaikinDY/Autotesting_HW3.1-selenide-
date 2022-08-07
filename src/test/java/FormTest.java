5dbc958fbe2e510a10b06cfe8a010a672f588bc5
RuzaikinDY <Devilwind@mail.ru>
2022-08-07 08:58:01 +0300

    Change name

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -12,15 +12,10 @@ public class FormTest {
         open("http://localhost:9999/");
     }
 
-<<<<<<< HEAD
-
-=======
- 
->>>>>>> e88fc076dc0f6a82af9493550e3aebcdea04c354
     @Test
     void shouldSendForm() {
         SelenideElement form = $(".form");
-        form.$("[data-test-id='name'] input").setValue("Иванов Иван");
+        form.$("[data-test-id='name'] input").setValue("Иванов-Петров Иван");
         form.$("[data-test-id='phone'] input").setValue("+79271520804");
         form.$("[data-test-id='agreement']").click();
         form.$("button").click();
